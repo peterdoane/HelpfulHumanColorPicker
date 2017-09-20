016c0b94aa00d38378985b3d199e81c8576bb3e1
Peter Doane <peterdoane@gmail.com>
2017-09-19 17:29:27 -0700

    css

--- src/ColorDetail/index.jsx
+++ src/ColorDetail/index.jsx
@@ -17,11 +17,13 @@ class ColorDetail extends Component {
       <div className="detailContainer">
         <div className="primaryColorItem">
           <Swatch color={this.props.color}/>
-          <button onClick={() => {this.props.onSelect(null)}}>Clear</button>
         </div>
+
         <div className="shades">
           {swatches}
         </div>
+
+        <button onClick={() => {this.props.onSelect(null)}}>Clear</button>
       </div>
     );
   }
